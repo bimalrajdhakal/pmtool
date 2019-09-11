5f031eea04dbd7178cac8668b84544d8b04f058e
unknown <sharmparu290@gmail.com>
2019-09-11 16:42:22 +0545

    hhhh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,14 +34,15 @@
         </div>
         <div class="card">
             <div class="body">
-                <form id="sign_in" method="POST">
+                <form id="sign_in" method="POST" action="{{route('login')}}">
+                @csrf
                     <div class="msg">Sign in to start your session</div>
                     <div class="input-group">
                         <span class="input-group-addon">
                             <i class="material-icons">person</i>
                         </span>
                         <div class="form-line">
-                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                            <input type="text" class="form-control" name="email" placeholder="Username" required autofocus>
                         </div>
                     </div>
                     <div class="input-group">
@@ -53,22 +54,14 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-xs-8 p-t-5">
-                            <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
-                            <label for="rememberme">Remember Me</label>
-                        </div>
-                        <div class="col-xs-4">
-                            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
-                        </div>
+                        
+                        <button type="submit" class="btn btn-block bg-pink waves-effect">
+                                    {{ __('Login') }}
+                        </button>
+                            
+                        
                     </div>
-                    <div class="row m-t-15 m-b--20">
-                        <div class="col-xs-6">
-                            <a href="sign-up.html">Register Now!</a>
-                        </div>
-                        <div class="col-xs-6 align-right">
-                            <a href="forgot-password.html">Forgot Password?</a>
-                        </div>
-                    </div>
+
                 </form>
             </div>
         </div>
